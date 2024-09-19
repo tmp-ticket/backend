32162a20347ff5988e312dd2a91776fd33ad361f
animalman33 <animalman3333@gmail.com>
2024-09-19 18:39:24 -0400

    Changed language of /HelloWorld log

--- routes/routes.go
+++ routes/routes.go
@@ -26,7 +26,7 @@ Basic hello world function that recieves a request from the http server in net/h
 that a request was recieved and logged to the console and sends back "Hello World!" to the requester.
 */
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	log.Println("Recieved Request on /HelloWorld")
+	log.Println("Recieved Request at /HelloWorld")
 
 	io.WriteString(w, "Hello World!")
 }
