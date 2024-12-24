5cbf079dc89c9df35c8ef3900942bfa2c608ab27
animalman33 <animalman3333@gmail.com>
2024-12-24 10:04:15 -0500

    added testing through docker compose

--- routes/routes_test.go
+++ routes/routes_test.go
@@ -6,7 +6,6 @@ func TestServerSetup(t *testing.T) {
 	Addr := ":8080"
 
 	server := ServerSetup(Addr)
-
 	if server.Addr != Addr {
 		t.Errorf("Server address %s does not equal the correct value %s", server.Addr, Addr)
 	}
