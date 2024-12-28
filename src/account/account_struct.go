af790a0d26bafc69ee9ec45e412bb5340fbd5cac
animalman33 <animalman3333@gmail.com>
2024-12-28 15:27:11 -0500

    account struct constructor created with error checking

--- src/account/account_struct.go
+++ src/account/account_struct.go
@@ -1,9 +1,30 @@
 package account
 
+import (
+	"errors"
+	"net/mail"
+)
+
 type Account struct {
 	email   string
 	id      int
 	is_auth bool
+}
+
+func NewAccountStruct(email string, id int, auth bool) (*Account, error) {
+	if email == "" {
+		return nil, errors.New("no email address provided")
+	}
+	_, mailCheck := mail.ParseAddress(email)
+
+	if mailCheck != nil {
+		return nil, errors.New("email provided is invalid")
+	}
+
+	if id < 1 {
+		return nil, errors.New("id is invalid")
+	}
+	return &Account{email: email, id: id, is_auth: auth}, nil
 }
 
 func (account Account) IsAuth() bool {
