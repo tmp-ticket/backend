08eadec2fd9a94dfc844f72e5ffe0956a6eb51b0
animalman33 <animalman3333@gmail.com>
2024-12-29 17:15:48 -0500

    added logging to login route

--- src/routes/AuthRoute.go
+++ src/routes/AuthRoute.go
@@ -1,40 +1,57 @@
 package routes
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 
 	"github.com/tmp-ticket/backend/src/account"
 )
 
 type AccountInfo struct {
-	email    string
-	password string
+	Email    string
+	Password string
 }
 
 func AuthUser(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "POST" {
-
 		body, err := io.ReadAll(r.Body)
 
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			slog.Log(context.Background(), slog.LevelInfo, err.Error())
 			return
 		}
 
 		var accountInfo AccountInfo
 
-		err = json.Unmarshal(body, accountInfo)
-		verifyAccount, err := account.AuthAccount(accountInfo.email, accountInfo.password)
+		err = json.Unmarshal(body, &accountInfo)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			slog.Log(context.Background(), slog.LevelInfo, err.Error())
+			return
+		}
+		verifyAccount, err := account.AuthAccount(accountInfo.Email, accountInfo.Password)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			slog.Log(context.Background(), slog.LevelError, err.Error())
+			return
+		}
 
 		if !verifyAccount.IsAuth() {
 			w.WriteHeader(http.StatusForbidden)
+			slog.Log(context.Background(), slog.LevelInfo, fmt.Sprintf("User %s failed to authenticate", accountInfo.Email))
+			return
 		}
 
 		http.Redirect(w, r, "/home", http.StatusMovedPermanently)
-
+		slog.Log(context.Background(), slog.LevelInfo, fmt.Sprintf("User %s successfully authenticated", accountInfo.Email))
 		return
 	} else {
 		return
