54f70db3f21ac4384bcdc8057c0f2fd81fa2567f
animalman33 <animalman3333@gmail.com>
2024-12-29 17:42:44 -0500

    added testing for database setup

---
+++ src/database/AccountDBCreate.go
@@ -0,0 +1,5 @@
+package database
+
+func CreateAccountDB() error {
+	return nil
+}
---
+++ src/database/DBConnect_test.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"testing"
+)
+
+func TestDBConnection(t *testing.T) {
+	err := SetupDBConnection()
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if DB_POOL == nil {
+		t.Errorf("the database pool is nil after calling setup function")
+	}
+}
