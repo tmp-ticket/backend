6c5c23fedbb200e63f87e9f5f7f4ce57603b5724
animalman33 <animalman3333@gmail.com>
2024-12-29 20:16:55 -0500

    starteed database createa account

--- src/database/AccountDBCreate.go
+++ src/database/AccountDBCreate.go
@@ -1,5 +1,17 @@
 package database
 
+import (
+	"errors"
+	"log/slog"
+)
+
 func CreateAccountDB() error {
+
+	_, err := DB_POOL.GetConn()
+	if err != nil {
+		slog.Error(err.Error())
+
+		return errors.New("Database connection has failed")
+	}
 	return nil
 }
