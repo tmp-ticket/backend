ee0af46d17be27a5e1cc82a46a641394b5f1b219
animalman33 <animalman3333@gmail.com>
2024-12-29 20:16:27 -0500

    added better error info to database connection test

--- src/database/DBConnect_test.go
+++ src/database/DBConnect_test.go
@@ -8,7 +8,7 @@ func TestDBConnection(t *testing.T) {
 	err := SetupDBConnection()
 
 	if err != nil {
-		t.Errorf(err.Error())
+		t.Errorf("recieved error on connection: %s", err.Error())
 	}
 
 	if DB_POOL == nil {
