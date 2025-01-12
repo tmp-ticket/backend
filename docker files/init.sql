3b6e3828a262278515f6dfc45bd31e85ca4a0d3b
animalman33 <animalman3333@gmail.com>
2025-01-12 16:00:28 -0500

    added indexing to test server email column in accounts table

--- docker files/init.sql
+++ docker files/init.sql
@@ -1,5 +1,7 @@
 CREATE TABLE accounts (
     ID SERIAL PRIMARY KEY,
-    email varchar(320),
+    email varchar(320) UNIQUE,
     password varchar(72)
-)
+);
+
+CREATE INDEX accounts_email_index on accounts using hash(email);
